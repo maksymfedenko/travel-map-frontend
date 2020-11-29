97c0f0d58c2584b0a8931e0bf170e65f141caebd
Maksym Fedenko <maksym.fedenko@gmail.com>
2020-11-29 18:34:27 +0200

    Initial project setup

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 80,
+  singleQuote: true,
+  trailingComma: 'all',
+  useTabs: false,
+  semi: true,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+};
